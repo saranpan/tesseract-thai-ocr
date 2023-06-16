f0d3eefecf43e093c57e4d56433a2de06714d531
Wallik2 <runpan4edu@gmail.com>
2023-06-16 17:25:53 +0700

    first commit

--- app.py
+++ app.py
@@ -3,39 +3,8 @@ from PIL import Image
 import pytesseract
 import streamlit as st
 import os
-import PyPDF2
+from pdf2image import convert_from_path
 from PIL import Image
-
-def convert_pdf_to_images(pdf_path):
-    images = []
-    
-    with open(pdf_path, "rb") as f:
-        reader = PyPDF2.PdfFileReader(f)
-        
-        for page_num in range(reader.numPages):
-            page = reader.getPage(page_num)
-            page_data = page.extractText()
-            
-            # Skip empty pages
-            if not page_data:
-                continue
-            
-            # Set a suitable DPI value for the image quality
-            dpi = 300
-            scale_factor = dpi / 72.0
-            page_scale = (scale_factor, scale_factor)
-            
-            # Convert the PDF page to an image
-            image = page.extract_images()[0]["image"]
-            width, height = image["width"], image["height"]
-            
-            pil_image = Image.frombytes("RGB", (width, height), image["data"])
-            pil_image = pil_image.resize((int(width*scale_factor), int(height*scale_factor)), Image.ANTIALIAS)
-            
-            images.append(pil_image)
-    
-    return images
-
 
 
 def main():
@@ -58,14 +27,12 @@ def main():
         elif file_type in ["pdf"]:
             with st.spinner("Transcribing PDF .."):
                 # Example usage
-                output_images = convert_pdf_to_images(temp_img_path)
+                images = convert_from_path(temp_img_path)
 
-                # Save the images
-                for i, img in enumerate(output_images):
+                for i in range(len(images)):
                     st.write(f"Page {i} :")
                     text = pytesseract.image_to_string(img, lang='tha')
                     st.write(text)
-                
                 
 
 if __name__ == "__main__":
